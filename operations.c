396bbbfe091db19595d19b5881b590696a721376
Ali Youssef <aliucef01@gmail.com>
2026-01-01 20:03:10 +0200

    new algo

--- operations.c
+++ operations.c
@@ -1,61 +1,102 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   operations.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alyousse <alyousse@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/26 13:12:38 by alyousse          #+#    #+#             */
-/*   Updated: 2025/11/26 17:46:19 by alyousse         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "push_swap_operations.h"
 
-void	swap(t_list *stack)
+void	swap(t_list *stack, char which, t_ops *ops)
 {
 	int	swapper;
 
 	if (!stack || !stack->next)
-		return ;
+		return;
 	swapper = stack->content;
 	stack->content = stack->next->content;
 	stack->next->content = swapper;
+	if (which == 'a')
+	{
+		ft_printf(1, "sa\n");
+		ops->sa++;
+	}
+	else if (which == 'b')
+	{
+		ft_printf(1, "sb\n");
+		ops->sb++;
+	}
 }
 
-void	swap_both(t_list *stack_a, t_list *stack_b)
+void	swap_both(t_list *stack_a, t_list *stack_b, t_ops *ops)
 {
-	swap(stack_a);
-	swap(stack_b);
+	int	swapper;
+
+	if (stack_a && stack_a->next)
+	{
+		swapper = stack_a->content;
+		stack_a->content = stack_a->next->content;
+		stack_a->next->content = swapper;
+	}
+	if (stack_b && stack_b->next)
+	{
+		swapper = stack_b->content;
+		stack_b->content = stack_b->next->content;
+		stack_b->next->content = swapper;
+	}
+	ft_printf(1, "ss\n");
+	ops->ss++;
 }
 
-void	rotate(t_list **stack)
+void	rotate(t_list **stack, char which, t_ops *ops)
 {
 	t_list	*last;
 	t_list	*first;
 
 	if (!stack || !*stack || !(*stack)->next)
-		return ;
+		return;
 	first = *stack;
 	*stack = first->next;
 	first->next = NULL;
 	last = ft_lstlast(*stack);
 	last->next = first;
+	if (which == 'a')
+	{
+		ft_printf(1, "ra\n");
+		ops->ra++;
+	}
+	else if (which == 'b')
+	{
+		ft_printf(1, "rb\n");
+		ops->rb++;
+	}
 }
 
-void	rotate_both(t_list **stack_a, t_list **stack_b)
+void	rotate_both(t_list **stack_a, t_list **stack_b, t_ops *ops)
 {
-	rotate(stack_a);
-	rotate(stack_b);
+	t_list	*last;
+	t_list	*first;
+
+	if (stack_a && *stack_a && (*stack_a)->next)
+	{
+		first = *stack_a;
+		*stack_a = first->next;
+		first->next = NULL;
+		last = ft_lstlast(*stack_a);
+		last->next = first;
+	}
+	if (stack_b && *stack_b && (*stack_b)->next)
+	{
+		first = *stack_b;
+		*stack_b = first->next;
+		first->next = NULL;
+		last = ft_lstlast(*stack_b);
+		last->next = first;
+	}
+	ft_printf(1, "rr\n");
+	ops->rr++;
 }
 
-void	reverse_rotate(t_list **stack)
+void	reverse_rotate(t_list **stack, char which, t_ops *ops)
 {
 	t_list	*before_last;
 	t_list	*last;
 
 	if (!stack || !*stack || !(*stack)->next)
-		return ;
+		return;
 	before_last = *stack;
 	while (before_last->next->next)
 		before_last = before_last->next;
@@ -63,4 +104,65 @@ void	reverse_rotate(t_list **stack)
 	before_last->next = NULL;
 	last->next = *stack;
 	*stack = last;
+	if (which == 'a')
+	{
+		ft_printf(1, "rra\n");
+		ops->rra++;
+	}
+	else if (which == 'b')
+	{
+		ft_printf(1, "rrb\n");
+		ops->rrb++;
+	}
+}
+
+void	reverse_rotate_both(t_list **stack_a, t_list **stack_b, t_ops *ops)
+{
+	t_list	*before_last;
+	t_list	*last;
+
+	if (stack_a && *stack_a && (*stack_a)->next)
+	{
+		before_last = *stack_a;
+		while (before_last->next->next)
+			before_last = before_last->next;
+		last = before_last->next;
+		before_last->next = NULL;
+		last->next = *stack_a;
+		*stack_a = last;
+	}
+	if (stack_b && *stack_b && (*stack_b)->next)
+	{
+		before_last = *stack_b;
+		while (before_last->next->next)
+			before_last = before_last->next;
+		last = before_last->next;
+		before_last->next = NULL;
+		last->next = *stack_b;
+		*stack_b = last;
+	}
+	ft_printf(1, "rrr\n");
+	ops->rrr++;
+}
+
+void	push(t_list **stack_a, t_list **stack_b, char which, t_ops *ops)
+{
+	t_list	*swap;
+
+	if (!*stack_a || !stack_a)
+		return;
+	swap = *stack_a;
+	*stack_a = (*stack_a)->next;
+	swap->next = *stack_b;
+	*stack_b = swap;
+	if (which == 'b')
+	{
+		ft_printf(1, "pb\n");
+		ops->pb++;
+	}
+	else
+	{
+		ft_printf(1, "pa\n");
+		ops->pa++;
+	}
 }
--- simple_sorting.c
+++ simple_sorting.c
@@ -1,106 +1,324 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   simple_sorting.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alyousse <alyousse@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/26 18:10:44 by alyousse          #+#    #+#             */
-/*   Updated: 2025/11/27 13:13:57 by alyousse         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "sorting_header.h"
+#include <stdlib.h>
 
-#include "push_swap_operations.h"
-
-// bubble sort (2000+ operations for 100 element)
-void	simple_sort(t_list *stack_a)
+static int	*stack_to_array(t_list *stack, int *size)
 {
-	t_list	*temp_head_holder;
-	int counter;
-
-	counter = 0;
-	temp_head_holder = stack_a;
-	while (stack_a->next)
+	*size = ft_lstsize(stack);
+	int *arr = malloc(sizeof(int) * (*size));
+	if (!arr)
+		return NULL;
+	
+	int i = 0;
+	while (stack)
 	{
-		if (stack_a->content > stack_a->next->content)
-		{
-			swap(stack_a);
-			ft_printf("sa\n");
-			counter++;
-			stack_a = temp_head_holder;
-		}
-		stack_a = stack_a->next;
+		arr[i++] = stack->content;
+		stack = stack->next;
 	}
-	stack_a = temp_head_holder;
-	//ft_printf("finished\n");
-	//ft_printf("counter :%d\n", counter);
+	return arr;
 }
-// selection sort
-int	selection_sort_operation(t_list **stack_a , t_list **stack_b)
-{
-	int		min;
-	int		path;
-	int		counter;
-	t_list	*head_holder;
-	int		which_reverse;
-	int		ops_counter;
 
-	ops_counter = 0;
-	which_reverse = 0;
-	counter = 0;
-	path = 0;
-	head_holder = *stack_a;
-	min = (*stack_a)->content;
-	while (*stack_a)
+static int	*find_lis(int *arr, int n, int *lis_len)
+{
+	if (n == 0)
 	{
-		if((*stack_a)->content < min)
-		{
-			min = (*stack_a)->content;
-			path = counter;
-		}
-		counter++;
-		*stack_a = (*stack_a)->next;
+		*lis_len = 0;
+		return NULL;
 	}
-	*stack_a = head_holder;
-	if (path > counter / 2)
+	
+	int *dp = malloc(sizeof(int) * n);
+	int *parent = malloc(sizeof(int) * n);
+	
+	if (!dp || !parent)
 	{
-		path = counter - path;
-		which_reverse = 1;
+		free(dp);
+		free(parent);
+		return NULL;
 	}
-	while (path--)
+	
+	for (int i = 0; i < n; i++)
 	{
-		if(which_reverse)
+		dp[i] = 1;
+		parent[i] = -1;
+	}
+	
+	int max_len = 1;
+	int max_idx = 0;
+	
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
 		{
-			reverse_rotate(stack_a);
-			 ft_printf("rra\n");
-			ops_counter++;
+			if (arr[j] < arr[i] && dp[j] + 1 > dp[i])
+			{
+				dp[i] = dp[j] + 1;
+				parent[i] = j;
+			}
 		}
+		if (dp[i] > max_len)
+		{
+			max_len = dp[i];
+			max_idx = i;
+		}
+	}
+	
+	int *lis = malloc(sizeof(int) * max_len);
+	if (!lis)
+	{
+		free(dp);
+		free(parent);
+		return NULL;
+	}
+	
+	int idx = max_idx;
+	for (int i = max_len - 1; i >= 0; i--)
+	{
+		lis[i] = idx;
+		idx = parent[idx];
+	}
+	
+	*lis_len = max_len;
+	free(dp);
+	free(parent);
+	return lis;
+}
+
+static int	*create_lis_set(int *lis, int lis_len, int max_index)
+{
+	int *set = calloc(max_index, sizeof(int));
+	if (!set)
+		return NULL;
+	
+	for (int i = 0; i < lis_len; i++)
+		set[lis[i]] = 1;
+	
+	return set;
+}
+
+static int	find_insert_position(t_list *a, int value)
+{
+	if (!a)
+		return 0;
+	
+	int size = ft_lstsize(a);
+	
+	t_list *tmp = a;
+	int min_val = tmp->content;
+	int max_val = tmp->content;
+	int min_pos = 0;
+	
+	int i = 0;
+	while (tmp)
+	{
+		if (tmp->content < min_val)
+		{
+			min_val = tmp->content;
+			min_pos = i;
+		}
+		if (tmp->content > max_val)
+			max_val = tmp->content;
+		tmp = tmp->next;
+		i++;
+	}
+	
+	if (value < min_val || value > max_val)
+		return min_pos;
+	
+	tmp = a;
+	i = 0;
+	int best_pos = min_pos;
+	int best_val = min_val - 1;
+	
+	while (tmp)
+	{
+		if (tmp->content < value && tmp->content > best_val)
+		{
+			best_val = tmp->content;
+			best_pos = (i + 1) % size;
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	
+	return best_pos;
+}
+
+static int	get_rotation_cost(int pos, int size)
+{
+	int forward = pos;
+	int backward = size - pos;
+	return (forward < backward) ? forward : backward;
+}
+
+static void	rotate_to_pos(t_list **stack, int pos, char which, t_ops *ops)
+{
+	int size = ft_lstsize(*stack);
+	if (pos == 0)
+		return;
+	
+	int forward = pos;
+	int backward = size - pos;
+	
+	if (forward <= backward)
+	{
+		while (pos-- > 0)
+			rotate(stack, which, ops);
+	}
+	else
+	{
+		while (pos++ < size)
+			reverse_rotate(stack, which, ops);
+	}
+}
+
+static int	find_cheapest_move(t_list *a, t_list *b, int *out_pos_a)
+{
+	int size_a = ft_lstsize(a);
+	int size_b = ft_lstsize(b);
+	int min_cost = 2147483647;
+	int best_pos_b = 0;
+	int best_pos_a = 0;
+	
+	t_list *tmp_b = b;
+	int pos_b = 0;
+	
+	while (tmp_b)
+	{
+		int value = tmp_b->content;
+		int pos_a = find_insert_position(a, value);
+		
+		int cost_a = get_rotation_cost(pos_a, size_a);
+		int cost_b = get_rotation_cost(pos_b, size_b);
+		
+		int fa = pos_a, ba = size_a - pos_a;
+		int fb = pos_b, bb = size_b - pos_b;
+		
+		int total_cost;
+		if ((fa <= ba && fb <= bb) || (fa > ba && fb > bb))
+			total_cost = (cost_a > cost_b) ? cost_a : cost_b;
 		else
+			total_cost = cost_a + cost_b;
+		
+		if (total_cost < min_cost)
 		{
-			rotate(stack_a);
-			ft_printf("ra\n");
-			ops_counter++;
+			min_cost = total_cost;
+			best_pos_b = pos_b;
+			best_pos_a = pos_a;
 		}
+		
+		tmp_b = tmp_b->next;
+		pos_b++;
 	}
-	push(stack_a, stack_b);
-	ft_printf("pa\n");
-	ops_counter++;
-	return ops_counter;
+	
+	*out_pos_a = best_pos_a;
+	return best_pos_b;
 }
 
-void	selection_sort(t_list **stack_a, t_list **stack_b)
+static void	execute_move(t_list **a, t_list **b, int pos_a, int pos_b, t_ops *ops)
 {
-	int ops_counter = 0;
-	while(*stack_a)
+	int size_a = ft_lstsize(*a);
+	int size_b = ft_lstsize(*b);
+	
+	int fa = pos_a, ba = size_a - pos_a;
+	int fb = pos_b, bb = size_b - pos_b;
+	
+	if (fa <= ba && fb <= bb)
 	{
-		ops_counter+=selection_sort_operation(stack_a, stack_b);
+		while (pos_a > 0 && pos_b > 0)
+		{
+			rotate_both(a, b, ops);
+			pos_a--;
+			pos_b--;
+		}
 	}
+	else if (fa > ba && fb > bb)
+	{
+		while (pos_a < size_a && pos_b < size_b)
+		{
+			reverse_rotate_both(a, b, ops);
+			pos_a++;
+			pos_b++;
+		}
+	}
+	
+	rotate_to_pos(a, pos_a, 'a', ops);
+	rotate_to_pos(b, pos_b, 'b', ops);
+	push(b, a, 'a', ops);
+}
+
+static void	align_stack(t_list **a, t_ops *ops)
+{
+	if (!*a)
+		return;
+	
+	int min = (*a)->content;
+	int min_pos = 0;
+	
+	t_list *tmp = *a;
+	int i = 0;
+	
+	while (tmp)
+	{
+		if (tmp->content < min)
+		{
+			min = tmp->content;
+			min_pos = i;
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	
+	rotate_to_pos(a, min_pos, 'a', ops);
+}
+
+void	selection_sort(t_list **stack_a, t_list **stack_b, t_ops *ops)
+{
+	int n = ft_lstsize(*stack_a);
+	if (n <= 1)
+		return;
+	
+	int size;
+	int *arr = stack_to_array(*stack_a, &size);
+	if (!arr)
+		return;
+	
+	int lis_len;
+	int *lis = find_lis(arr, size, &lis_len);
+	if (!lis)
+	{
+		free(arr);
+		return;
+	}
+	
+	int *lis_set = create_lis_set(lis, lis_len, size);
+	if (!lis_set)
+	{
+		free(arr);
+		free(lis);
+		return;
+	}
+	
+	int idx = 0;
+	int total = size;
+	
+	while (idx < total)
+	{
+		if (lis_set[idx])
+			rotate(stack_a, 'a', ops);
+		else
+			push(stack_a, stack_b, 'b', ops);
+		idx++;
+	}
+	
+	free(arr);
+	free(lis);
+	free(lis_set);
+	
 	while (*stack_b)
 	{
-		push(stack_b, stack_a);
-		ft_printf("pb\n");
-		ops_counter++;
+		int pos_a;
+		int pos_b = find_cheapest_move(*stack_a, *stack_b, &pos_a);
+		execute_move(stack_a, stack_b, pos_a, pos_b, ops);
 	}
-	ft_printf("final operations number : %d \n" ,ops_counter++);
+	
+	align_stack(stack_a, ops);
 }
-
